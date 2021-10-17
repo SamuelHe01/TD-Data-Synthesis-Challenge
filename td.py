1667198af036f900e4bb8bfee7a4496ca73372e9
ericlee2-eng <85694486+ericlee2-eng@users.noreply.github.com>
2021-10-17 08:29:05 -0500

    Add files via upload

--- td.py
+++ td.py
@@ -1,41 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 import xlrd
-import xlwt
 from xlwt import Workbook
 
+#holds links from webcrawl
 links = set()
 
+#takes url string and converts it to soup objeect using html5lib interpreter
 def url_to_soup(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html5lib')
     return soup
 
+#returns list of all instances of tr in a soup
 def find_all_tr(soup):
     data = soup.find_all('tr')
     return data
 
+#returns list of all instances of td in a soup
 def find_all_td(soup):
     data = soup.find_all('td')
     return data
 
+#does a webcrawl to find links recurively, the # of level it searches is based on count, base can be a keyword required for links to curate the final set
 def search_links(soup, count, base):
-    if(count == 0 or len(links)>500):
+    if(count == 0 or len(links)>500): #if # of links is greater than 500 or reached end of recursion (base case)
         return
     #print(soup.find_all('a', href=True))
-    for a in soup.find_all('a', href=True):
+    for a in soup.find_all('a', href=True): #for every href in soup we try to find the base and if it exists then add it to set links
         if(a['href'].find(base) == -1):
             continue
         links.add(a['href'])
-        print(a['href'])
         try:
-            search_links(url_to_soup(a['href']), count-1, base)
+            search_links(url_to_soup(a['href']), count-1, base)#attempt a recursive call but if url doesn't work or smth just move to next
         except:
             continue
-    #print(len(links))
     return links
 
+#gets list of lot names from parking site
 def search_lots():
     link = 'https://transport.tamu.edu/Parking/faqpermit/info-offcampus.aspx'
     soup = url_to_soup(link)
@@ -46,6 +48,7 @@ def search_lots():
             names.add(p.text)
     return (names)
 
+#gets paid parking rates from parking site
 def get_garage_rates():
     link = 'https://transport.tamu.edu/Parking/visitor.aspx'
     soup = url_to_soup(link)
@@ -58,6 +61,7 @@ def get_garage_rates():
         finallist.append(n)
     return finallist
 
+#reads from a excel sheet we curated
 def readfromxl():
     loc =r'C:\Users\ericlee2\Downloads\lotlist.xls'
     wb = xlrd.open_workbook(loc)
@@ -66,16 +70,27 @@ def readfromxl():
     lotnames = []
     goodrows = []
     for i in range(sheet.nrows-3):
-       # print(type(sheet.cell_value(i+3,2)))
         if(type(sheet.cell_value(i+2,2)) == float and sheet.cell_value(i+2,2)>0):
             lotnames.append(sheet.cell_value(i+2, 0))
             goodrows.append(i+3)
     return lotnames
 
+#writes data into excel sheet
 def writedata_xl(lotnames):
-    wb = Workbook()
-    sheet1 = wb.add_sheet('Sheet 1')
-    weekday = True
+    wb = Workbook() #creates excel workbook
+    sheet1 = wb.add_sheet('Sheet 1') #makes a sheet called Sheet 1
+    sheet2 = wb.add_sheet('PAID PARKING') #makes a sheet called "PAID PARKING"
+    #puts garage rates into 'PAID PARKING'
+    garage_rate = get_garage_rates()
+    ratelinecouter = 0
+    shift = 0
+    for item in garage_rate:
+        sheet2.write(ratelinecouter,shift, item)
+        shift += 1
+        if(shift == 3):
+            shift = 0
+            ratelinecouter += 1
+    #Puts lot names in excelsheet and preps other identifier columns
     linecounter = 1
     hour = 0
     ticker = 1
@@ -103,8 +118,10 @@ def writedata_xl(lotnames):
             hour += 1
             linecounter += 1
 
+    #saves all prep into an excel sheet called exmaples.xls
     wb.save('examples.xls')
 
+#webscrapes a list of permit names from trasport website
 def get_permits():
     link = 'https://transport.tamu.edu/Parking/faqpermit/info.aspx'
     soup = url_to_soup(link)
@@ -117,6 +134,7 @@ def get_permits():
             names.append(td_list[0].text)
     return(names)
 
+#natural language interpter that searches for key words referencing not avaiable parking and returns boolean
 def not_avail_parking_words(sentence):
     checkwords = ['unavailable','reserved', 'restricted', 'not available', 'not permitted', 'not open']
     for word in checkwords:
@@ -124,6 +142,7 @@ def not_avail_parking_words(sentence):
             return True
     return False
 
+#natural language interpreter that searches for key words referencing avaiable parking and return boolean
 def avail_parking_words(sentence):
     checkwords = ['available', 'permitted', 'open']
     for word in checkwords:
@@ -131,6 +150,7 @@ def avail_parking_words(sentence):
             return True
     return False
 
+#using the natural language interpreters to determine the availability status of a sentence
 def ambiguous_sentence_determiner(sentence):
     if(not_avail_parking_words(sentence)):
         return 'not available'
@@ -139,6 +159,7 @@ def ambiguous_sentence_determiner(sentence):
     else:
         return
 
+#webscrapes data about event parking from parking site
 def event_parking_lots():
     link = 'https://transport.tamu.edu/Parking/events/annual.aspx'
     soup = url_to_soup(link)
@@ -156,6 +177,7 @@ def event_parking_lots():
     print(ret)
     #print(sentences)
 
+#webscrapes site for any mention of lot or garage in the paragraph objects in a site
 def search_lots_and_garage(site):
     checkwords = ['lot', 'Lot', 'Lots', 'lots', 'Garage', 'garage']
     soup = url_to_soup(site)
